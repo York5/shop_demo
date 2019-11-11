2dbc282f343c3e69347adad319a3da94cb73f44e
York5 <mikhailvishin2@yandex.com>
2019-11-11 18:37:24 +0600

    Сатистика вышла костыльная и считает только колличество посещений. Код не динамичный увы, сразу не врубился как делать со словарями хранящимися в сессии, а когда понял что иду неверным путем уже времени не было все переделывать). Пока как то так получилось.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -9,9 +9,38 @@ from datetime import datetime, timedelta
 class SessionCounterMixin:
     def get(self, request, *args, **kwargs):
         if request.method == 'GET':
+            index_time = request.session.get('index_time', 0)
+            detail_time = request.session.get('detail_time', 0)
+            create_time = request.session.get('create_time', 0)
+            basket_time = request.session.get('basket_time', 0)
+            total_time = request.session.get('total_time', 0)
+
+            # request.session['stats_dict'] =  {}
+
             if request.path == '/':
                 index_visits = request.session.get('index_visits', 0)
                 request.session['index_visits'] = index_visits + 1
+                index_start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                request.session['index_start_time'] = index_start_time
+          
+            elif 'products' in request.path:
+                detail_visits = request.session.get('detail_visits', 0)
+                request.session['detail_visits'] = detail_visits + 1
+                detail_start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                request.session['detail_start_time'] = detail_start_time
+
+            elif request.path == '/product/create/':
+                create_product_visits = request.session.get('create_product_visits', 0)
+                request.session['create_product_visits'] = create_product_visits + 1
+                create_start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                request.session['create_start_time'] = create_start_time
+
+            elif request.path == '/basket/':
+                basket_visits = request.session.get('basket_visits', 0)
+                request.session['basket_visits'] = basket_visits + 1
+                basket_start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                request.session['basket_start_time'] = basket_start_time
+
         return super().get(request, *args, **kwargs)
 
 
@@ -20,28 +49,16 @@ class IndexView(SessionCounterMixin, ListView):
     template_name = 'index.html'
 
 
-class ProductView(DetailView):
+class ProductView(SessionCounterMixin, DetailView):
     model = Product
     template_name = 'product/detail.html'
 
-    def dispatch(self, request, *args, **kwargs):
-        if request.method == 'GET':
-            detail_visits = request.session.get('detail_visits', 0)
-            request.session['detail_visits'] = detail_visits + 1
-        return super().dispatch(request, *args, **kwargs)
 
-
-class ProductCreateView(CreateView):
+class ProductCreateView(SessionCounterMixin, CreateView):
     model = Product
     template_name = 'product/create.html'
     fields = ('name', 'category', 'price', 'photo')
     success_url = reverse_lazy('webapp:index')
-
-    def dispatch(self, request, *args, **kwargs):
-        if request.method == 'GET':
-            create_product_visits = request.session.get('create_product_visits', 0)
-            request.session['create_product_visits'] = create_product_visits + 1
-        return super().dispatch(request, *args, **kwargs)
 
 
 class BasketChangeView(View):
@@ -66,7 +83,7 @@ class BasketChangeView(View):
         return redirect(next_url)
 
 
-class BasketView(CreateView):
+class BasketView(SessionCounterMixin, CreateView):
     model = Order
     fields = ('first_name', 'last_name', 'phone', 'email')
     template_name = 'product/basket.html'
@@ -125,15 +142,6 @@ class BasketView(CreateView):
 
 class SessionStatsView(TemplateView):
     template_name = 'session_stats.html'
-    # def dispatch(self, request, *args, **kwargs):
-    #     if request.method == 'GET':
-    #         index_visits = request.session.get('index_visits', 0)
-    #         index_time = request.session.get('index_time', 0)
-    #
-    #         detail_visits = request.session.get('detail_visits', 0)
-    #         detail_time = request.session.get('detail_time', 0)
-    #
-    #     return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         kwargs['index_visits'] = self.request.session.get('index_visits', 0)
@@ -144,5 +152,8 @@ class SessionStatsView(TemplateView):
 
         kwargs['create_product_visits'] = self.request.session.get('create_product_visits', 0)
         kwargs['create_product_time'] = self.request.session.get('create_product_time', 0)
+
+        kwargs['basket_visits'] = self.request.session.get('basket_visits', 0)
+        kwargs['basket_time'] = self.request.session.get('basket_time', 0)
 
         return super().get_context_data(**kwargs)
